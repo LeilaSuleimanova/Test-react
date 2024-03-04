33ed5c8e797f8e07f73b6e483ace0976a254aa7b
Leila Suleimanova <leila_suleymanova@mail.ru>
2024-03-05 01:34:50 +0300

    Updates

---
+++ precache-manifest.2acccf800eda67f2812693be34b6e992.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0ad15414509bbd9b85f2edbebbeb82",
+    "url": "/Test-react/index.html"
+  },
+  {
+    "revision": "3bfa9b2c6a0c5a3e7d30",
+    "url": "/Test-react/static/css/2.618267d1.chunk.css"
+  },
+  {
+    "revision": "15b4cd319a85de4cc2d8",
+    "url": "/Test-react/static/css/main.5195de0b.chunk.css"
+  },
+  {
+    "revision": "3bfa9b2c6a0c5a3e7d30",
+    "url": "/Test-react/static/js/2.405c7cd2.chunk.js"
+  },
+  {
+    "revision": "db4976e2c3fc371be79e",
+    "url": "/Test-react/static/js/3.1af95c37.chunk.js"
+  },
+  {
+    "revision": "15b4cd319a85de4cc2d8",
+    "url": "/Test-react/static/js/main.912a15fc.chunk.js"
+  },
+  {
+    "revision": "162c010c53d5b0fdc18b",
+    "url": "/Test-react/static/js/runtime~main.65abd536.js"
+  },
+  {
+    "revision": "5567d7e465775e9e49ab8f0a5508a642",
+    "url": "/Test-react/static/media/PT_Sans-Web-Bold.5567d7e4.ttf"
+  },
+  {
+    "revision": "f2eadd43911fbf66f2bf5c8b32f69724",
+    "url": "/Test-react/static/media/PT_Sans-Web-Regular.f2eadd43.ttf"
+  },
+  {
+    "revision": "f456b338f6041f6c610b2ec2ce5e6f34",
+    "url": "/Test-react/static/media/button-close.f456b338.svg"
+  },
+  {
+    "revision": "8497a51e197d209799a8aca20753c00d",
+    "url": "/Test-react/static/media/button-minus.8497a51e.svg"
+  },
+  {
+    "revision": "7f29c1be1d989486c14836d4f6661f01",
+    "url": "/Test-react/static/media/button-plus.7f29c1be.svg"
+  },
+  {
+    "revision": "e489a7634ebee851dfbbb554cfa7f432",
+    "url": "/Test-react/static/media/check-up.e489a763.png"
+  },
+  {
+    "revision": "6d7f30d3bb44f14c74df991c28c072e1",
+    "url": "/Test-react/static/media/close-mobile.6d7f30d3.svg"
+  },
+  {
+    "revision": "6d7f30d3bb44f14c74df991c28c072e1",
+    "url": "/Test-react/static/media/close-popup-mobile.6d7f30d3.svg"
+  },
+  {
+    "revision": "2a87bc4fc2f06b86856204bda92d69ad",
+    "url": "/Test-react/static/media/icon-location.2a87bc4f.svg"
+  },
+  {
+    "revision": "baf8ebc0ecf13c006a4f46b81e7f8441",
+    "url": "/Test-react/static/media/inst-icon.baf8ebc0.svg"
+  },
+  {
+    "revision": "2eaa3b881c18f65d936a8f73da1c2e12",
+    "url": "/Test-react/static/media/left-arrow.2eaa3b88.svg"
+  },
+  {
+    "revision": "c6d34798044bdb9542820d3efdca157d",
+    "url": "/Test-react/static/media/logo-footer.c6d34798.svg"
+  },
+  {
+    "revision": "cbbfacf617d86c810242ba4557a6a837",
+    "url": "/Test-react/static/media/logo.cbbfacf6.svg"
+  },
+  {
+    "revision": "ad3e94e31a04f8f14123b55800578614",
+    "url": "/Test-react/static/media/main-picture.ad3e94e3.png"
+  },
+  {
+    "revision": "9822204a9b674dc3bd226fecdd816cde",
+    "url": "/Test-react/static/media/mainpic-mobile.9822204a.png"
+  },
+  {
+    "revision": "b01b4e1dda663b071491c0202cc44292",
+    "url": "/Test-react/static/media/menu-icon.b01b4e1d.svg"
+  },
+  {
+    "revision": "871f6c8d0ba95bfbad874f393b4f2ccb",
+    "url": "/Test-react/static/media/right-arrow-active.871f6c8d.svg"
+  },
+  {
+    "revision": "1a793cb516771b7d398aaf66eaa3c954",
+    "url": "/Test-react/static/media/right-arrow.1a793cb5.svg"
+  },
+  {
+    "revision": "432132426bac6b89f5b6616e3139e94c",
+    "url": "/Test-react/static/media/slider-mobile.43213242.png"
+  },
+  {
+    "revision": "47b1daf1ebb7ba18848a96c0c7c69d26",
+    "url": "/Test-react/static/media/telegram-icon.47b1daf1.svg"
+  },
+  {
+    "revision": "599e60817955845afb86cc39e5e9803f",
+    "url": "/Test-react/static/media/whatsapp-footer.599e6081.svg"
+  }
+]);
